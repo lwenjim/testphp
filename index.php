95e193be1e2a5d4af913074a91147eb4a960cd68
liuwenjin <liuwenjin@jspp.com>
2024-05-07 17:32:34 +0800

    feat: delete format.php

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-$ch = curl_init("http://localhost:8080/ping");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-// curl_exec($ch);
-curl_close($ch);
+echo file_get_contents("http://www.baidu.com");
